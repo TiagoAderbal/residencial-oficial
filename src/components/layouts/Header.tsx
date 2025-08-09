1073a167b5c9ee9b4dfff6faf8ff14aae69dff5a
Tiago Aderbal <tiago.francisco@estadao.com>
2025-08-08 23:46:25 -0300

    primeiras modificacoes no projeto

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,6 +1,5 @@
 import { handleSignOut } from "@/lib/server/auth";
 import { useAuthStore } from "@/store/authStore";
-import { useChatStore } from "@/store/chatStore";
 import { useTheme } from "next-themes";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -23,13 +22,11 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 export const Header = () => {
   const { setTheme } = useTheme();
   const { user, clearUser } = useAuthStore();
-  const { setChat, showChatsList, setShowChatsList } = useChatStore();
 
   const pathname = usePathname();
 
   const handleLogOut = () => {
     handleSignOut();
-    setChat(null);
     clearUser();
     toast.success("Deslogado com sucesso!", { position: "top-center" });
   };
@@ -59,14 +56,6 @@ export const Header = () => {
         </div>
 
         <div className="flex items-center gap-6">
-          <Button
-            className="flex lg:hidden"
-            size="icon"
-            onClick={() => setShowChatsList(!showChatsList)}
-          >
-            <Menu className="size-[1.2rem]" />
-            <span className="sr-only">Abrir/Fechar as conversas</span>
-          </Button>
 
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
@@ -123,20 +112,6 @@ export const Header = () => {
                 </DropdownMenuItem>
               </DropdownMenuContent>
             </DropdownMenu>
-          )}
-
-          {!user && pathname.startsWith("/auth") && (
-            <div>
-              {pathname !== "/auth/signin" ? (
-                <Button size="sm" asChild>
-                  <Link href="/auth/signin">Entrar</Link>
-                </Button>
-              ) : (
-                <Button size="sm" asChild>
-                  <Link href="/auth/signup">Registrar-se</Link>
-                </Button>
-              )}
-            </div>
           )}
         </div>
       </nav>
