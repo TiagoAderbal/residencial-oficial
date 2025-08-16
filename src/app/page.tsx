cafc4c4f1ee431366e39c7715a879d4642df096c
André Camargo <andre.camargo@msn.com>
2025-08-16 09:57:50 -0300

    Remove arquivos ignorados pelo .gitignore

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
 import { HomePage } from "@/components/Pages/Home";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home page",
+};
 
 const Home = () => <HomePage />;
 
